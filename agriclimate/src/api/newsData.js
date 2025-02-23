2425da91c85497ec3286e05d189aad301d17ca5c
SiddharthS15 <siddharthsadake@gmail.com>
2025-02-23 10:00:40 +0530

    Removed Climate Prredction Card

--- agriclimate/src/api/newsData.js
+++ agriclimate/src/api/newsData.js
@@ -1,6 +1,6 @@
 export async function fetchWeatherNews() {
     const API_KEY = "74b8c247dd0d45b7a7edd9f5bc37e692";  // Replace with your API Key
-    const response = await fetch(`https://newsapi.org/v2/everything?q=India agriculture OR Indian farming&language=en&apiKey=${API_KEY}`);
+    const response = await fetch(`https://newsapi.org/v2/everything?q=agriculture&language=en&apiKey=${API_KEY}`);
     const data = await response.json();
     
     if (data.articles) {
