6a1d91b9082276254fffde9e35cb1f1d788d4ae1
AbhayShankhapal <shankhapalabhay@gmail.com>
2025-02-23 10:22:54 +0530

    updated newsData

--- agriclimate/src/api/newsData.js
+++ agriclimate/src/api/newsData.js
@@ -1,8 +1,9 @@
 export async function fetchWeatherNews() {
     const API_KEY = "74b8c247dd0d45b7a7edd9f5bc37e692";  // Replace with your API Key
-    const response = await fetch(`https://newsapi.org/v2/everything?q=India agriculture OR Indian farming&language=en&apiKey=${API_KEY}`);
+    const query = encodeURIComponent("Indian agriculture OR farming OR crops OR harvest OR agribusiness OR organic farming");
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&language=en&apiKey=${API_KEY}`);
     const data = await response.json();
-    
+
     if (data.articles) {
         return data.articles.slice(0, 10).map(article => ({
             title: article.title,
@@ -10,6 +11,6 @@ export async function fetchWeatherNews() {
             source: article.source.name
         }));
     }
-    
+
     return [];
 }
